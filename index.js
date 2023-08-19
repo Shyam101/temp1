c8a56eca5756cc957ebeee830f01103556c9265e
Shyam101 <50941903+Shyam101@users.noreply.github.com>
2023-08-19 19:30:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,15 @@ const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
-app.use(bodyParser.json( extended = true ));
+app.use(bodyParser.json()); // Use bodyParser.json() without arguments
 
 app.listen(
     PORT,
-    () => console.log("Server listing on : " + PORT)
+    () => console.log("Server listening on : " + PORT)
 );
-
-app.get("/getRequest", (req, res) => {
-    res.send("Hello bro")
-});
 
 app.post("/postRequest", (req, res) => {
     console.log("Logging request:");
-    console.log(req);
-    console.log("Logging body:");
-    console.log(req.body);
-    res.send("Hello post")
+    console.log(req.body); // Log the parsed body
+    res.send("Hello post");
 });
