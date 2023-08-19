d3da8e234b5cb54de9d9d268831ac19fe59f325b
Shyam101 <50941903+Shyam101@users.noreply.github.com>
2023-08-19 17:28:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+app.use(bodyParser.json( extended = false ));
+
+app.listen(
+    PORT,
+    () => console.log("Server listing on : " + PORT)
+);
+
+app.get("/getRequest", (req, res) => {
+    res.send("Hello bro")
+});
+
+app.post("/postRequest", (req, res) => {
+    console.log(req.body);
+    res.send("Hello post")
+});
