d73b13cbf194034f0657bbb8a2d5e724df605b37
Shyam101 <50941903+Shyam101@users.noreply.github.com>
2023-08-22 12:06:56 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def log_post_request():
+    # Log the incoming POST request data to the terminal
+    request_data = request.data.decode('utf-8')
+    print("Received POST request data:")
+    print(request_data)
+
+    return 'Request logged successfully', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
